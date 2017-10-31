dcd9399654fd126161f76a2f51c501a1df789e16
Shaeli <eleonore.carpentier@insa-cvl.fr>
2017-10-31 19:20:57 +0100

    Code fonctionel- Sans commentaires

--- src/client/RepartiteurThread.java
+++ src/client/RepartiteurThread.java
@@ -14,121 +14,119 @@ import shared.ServerInterface;
 
 public class RepartiteurThread extends Thread {
 
-  // Stub du serveur avec lequel notre thread va communiquer.
   private ServerInterface stub;
-
-  // Booléen permettant de savoir si le calcul s'est déroulé sans erreurs ou non.
   private Boolean overload;
-
-  // Liste des opérations que doit faire notre thread.
   private ArrayList<Operation> task;
-
-  // Chaine sérialisée contenant les opérations à faire qui sera envoyé au serveur.
   private String serial_string;
-
-  // Booléen concernant l'état du thread (s'il est disponible ou non).
   private boolean busy;
-
-  // Booléen permettant de savoir s'il reste des opérations à effectuer.
-  // Tant que la pile d'opération n'est pas vide, on continue l'éxécution du thread.
   private boolean inprogress;
-
-  // Liste des résultats de nos opérations.
   private String[] results;
+  private boolean down;
 
 
-  /**
-   * Constructeur du thread.
-   *
-   * @param stub Stub du serveur avec lequel notre thread va communiquer.
-   */
-  RepartiteurThread(ServerInterface stub)
-  {
+  RepartiteurThread(ServerInterface stub) {
+
+
     this.stub = stub;
     this.overload = false;
     this.busy=false;
     this.inprogress=true;
+    this.down=false;
   }
 
   public void run()
   {
-    while(inprogress)
+    loop();
+  }
+
+    private void loop()
     {
+      while(inprogress)
+      {
+       try
+        {
+          Thread.sleep(0,1);
+        }
+        catch(Exception e){}
       if (busy)
       {
         traitement();
       }
     }
-  }
-
-  /**
-   * Méthode pour le traitement d'une sous liste d'opérations.
-   *
-   */
-  private void traitement()
-  {
-
-    // Envoie de la requête au serveur et récupération du résultat.
-    String tampon = "";
-    try
+    }
+    private void traitement()
     {
-      this.serial_string = Integer.toString(this.task.size());
-      for (Operation operation : this.task)
+      String tampon = "";
+      try
       {
-        this.serial_string += "&" + operation.getOperationName() + ":" + operation.getOperande();
-      }
-      tampon = stub.Calculer(this.serial_string);
-      if (!tampon.equals(null))
-      {
-        this.results = tampon.split("&");
-        // traitement des résultats reçus dans chaque instance d'opération.
-        for (int i = 0; i < this.results.length; i++)
+        this.serial_string = Integer.toString(this.task.size());
+        for (Operation operation : this.task)
         {
-           this.task.get(i).setResult(Integer.parseInt(this.results[i]));
-           this.task.get(i).setValidation();
+          this.serial_string += "&" + operation.getOperationName() + ":" + operation.getOperande();
         }
-      }
-      else
+        tampon = stub.Calculer(this.serial_string);
+        if (!tampon.equals("refus"))
+        {
+          this.results = tampon.split("&");
+          if (this.results.length == this.task.size())
+          {
+           try
+           {
+            Thread.sleep(0,1);
+            }
+           catch(Exception e){}
+            for (int i = 0; i < this.results.length; i++)
+             {
+               this.task.get(i).setResult(Integer.parseInt(this.results[i]));
+               this.task.get(i).setValidation();
+             }
+          }
+          
+        }else
+        {
+          System.out.println("Il faut baisser le taux");
+         this.overload = true;
+
+        }
+      }catch (RemoteException e)
       {
-       this.overload = true;
+       System.out.println("Probleme du cote du serveur : plus d'envoie a ce serveur."+Thread.currentThread().getName());
+       this.down=true;
+
       }
-      for (Operation o : task)
+      for (int i = 0; i < this.task.size(); i++)
       {
-        System.out.println(o.getResult());
+           this.task.get(i).setTreatment(true);
       }
+      this.busy=false;
     }
-    catch (RemoteException e)
+
+    public void setInprogress(Boolean value)
     {
-     System.out.println("Erreur: " + e.getMessage());
+      this.inprogress=value;
     }
-    this.busy=false;
-  }
 
-  /**
-   * Accesseur du booléen de progression de la tache globale.
-   * @param Boolean value progression de la tache globale (il reste des opératiohs ou non).
-   */
-  public void setInprogress(Boolean value)
-  {
-    this.inprogress = value;
-  }
+    public Boolean getBusy()
+    {
+      return this.busy;
+    }
 
-  /**
-   * Accesseur du booléen d'état du thread
-   * @return l'état actuel du thread.
-   */
-  public Boolean getBusy()
-  {
-    return this.busy;
-  }
-
-  /**
-   * Accesseur de la liste de tache d'opérations à faire par notre thread.
-   * @param ArrayList<Operation> task liste des opérations à faire.
-   */
-  public void setTask(ArrayList<Operation> task)
-  {
-    this.task = task;
-    this.busy=true;
-  }
+    public Boolean getDown()
+    {
+      return this.down;
+    }
+    public void setTask(ArrayList<Operation> task)
+    {
+      System.out.println(task.size());
+      this.task = task;
+      this.busy=true;
+    }
+    public Boolean getOverload()
+    {
+       return this.overload;
+    }
+    public void setOverload(Boolean value)
+    {
+       this.overload=value;
+    }
  }
