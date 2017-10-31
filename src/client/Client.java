677a90f947d6b91139dd85aa65d7fe48df7adc0c
Shaeli <eleonore.carpentier@insa-cvl.fr>
2017-10-31 19:18:59 +0100

    Code final fonctionnel- sans commentaires

--- src/client/Client.java
+++ src/client/Client.java
@@ -15,41 +15,29 @@ import java.util.concurrent.Executors;
 import shared.ServerInterface;
 
 public class Client {
-
-  // Type de sécurité pour notre Répartiteur : 0 = sécurisé / 1 = non sécurisé.
   private int _mode;
-
-  // Liste contenant les opérations à répartir entre nos différents clients.
-  private ArrayList<Operation> operations_stack;
-
-  // Liste contenant les opérations en cours de traitement par nos serveurs.
-  private List<Operation> in_progress_operations_stack;
-
-  // Hashmap contenant chaque threads (1 par serveur) avec la capacité associée.
+  private ArrayList<Operation> operations_stack; //operations a faire
+  private List<Operation> in_progress_operations_stack; //operations en traitement
   private HashMap<RepartiteurThread, Integer> threads;
-
-  // Résultat final de notre fichier d'opérations.
+  private HashMap<RepartiteurThread, ArrayList<Operation>> thread_op;
   private int result;
 
-
-	public static void main(String[] args)
-  {
-    HashMap<String,Integer> socket = new HashMap<String, Integer>();
+	public static void main(String[] args) {
     HashMap<HashMap<String,Integer>,Integer> servers = new HashMap<HashMap<String,Integer>,Integer>();//hashmap contenant les socket serveur et leur capacité
 
-    if (args.length != 2 && (Integer.parseInt(args[1]) != 0 || Integer.parseInt(args[1]) != 1))
-    {
+    if (args.length != 2 && (Integer.parseInt(args[1]) != 0 || Integer.parseInt(args[1]) != 1)) {
       System.out.println("Erreur : Nombre d'arguments incorrect \n\tUsage : ./client Fichier  Mode d'execution:(S = 0/NS = 1)\n");
     }
     else
     {
       try (BufferedReader lines = new BufferedReader(new FileReader("src/client/Servers.txt"))) {
         String line;
-        while ((line = lines.readLine()) != null)
-        {
+        while ((line = lines.readLine()) != null) {
+          HashMap<String,Integer> socket = new HashMap<String, Integer>();
           socket.put(line.split(":")[0], Integer.parseInt(line.split(":")[1]));
-          servers.put(socket, Integer.parseInt(line.split(":")[2]));
+          servers.put(socket,Integer.parseInt(line.split(":")[2])); 
         }
+       
       }
       catch (IOException e)
       {
@@ -60,72 +48,66 @@ public class Client {
     }
 	}
 
-  /**
-   * Constructeur du Répartirteur
-   * @param  HashMap<HashMap<String, Integer>, Integer>  servers  HashMap des serveurs disponibles ainsi que leur capacité respective.
-   * @param  String                  file          Fichier contenant la liste d'opérations à effectuer.
-   * @param  int                     mode          Mode de sécurité de notre répartirteur.
-   * @return                         Répartirteur.
-   */
-	public Client(HashMap<HashMap<String, Integer>, Integer> servers, String file, int mode)
-  {
-		super();
 
-    //Initialisation des arguments.
+	public Client(HashMap<HashMap<String, Integer>, Integer> servers, String file, int mode) {
+		super();
     this._mode = mode;
     this.operations_stack = new ArrayList<Operation>();
     this.in_progress_operations_stack = Collections.synchronizedList(new ArrayList<Operation>());
     this.threads = new HashMap<RepartiteurThread, Integer>();
+    this.thread_op = new HashMap<RepartiteurThread,ArrayList<Operation>>();
     this.result = 0;
-
 		if (System.getSecurityManager() == null)
 		{
 			System.setSecurityManager(new SecurityManager());
 		}
-
-    //Initialisation de la pile d'opérations à faire.
     FileToArray(file);
+    for (HashMap<String, Integer> socket : servers.keySet()) {
+        RepartiteurThread t = new RepartiteurThread(loadServerStub(socket));
+        threads.put(t, servers.get(socket));
+        thread_op.put(t, new ArrayList<Operation>());
+        t.start();
 
-    // Création des threads avec le stub d'un serveur ainsi que la capacité associée.
-    // Démarrage des threads qui vont être en attente d'opérations à effectuer.
-    for (HashMap<String, Integer> socket : servers.keySet())
-    {
-      RepartiteurThread t = new RepartiteurThread(loadServerStub(socket));
-      threads.put(t, servers.get(socket));
-      t.start();
-    }
+      }
 	}
 
 	private void run()
   {
-    ArrayList<Operation> task = new ArrayList<Operation>();
+    ArrayList<Operation> task;
     while(!operations_stack.isEmpty() || !in_progress_operations_stack.isEmpty())
     {
 
       //Traitement des threads : On vérifie leur état avant de leur envoyer une tache s'ils sont disponibles.
       for (RepartiteurThread thread : threads.keySet())
       {
-        if(!thread.getBusy())
+        if(!thread.getBusy() && !thread.getDown())
         {
+          if(thread.getOverload())
+          {
+             setCapacity(thread,false);
+          }else{
+             setCapacity(thread,true);
+          }
           if(!operations_stack.isEmpty())
           {
+            task=thread_op.get(thread);
             task.clear();
+
             for (int i = 0; i < threads.get(thread); i++)
             {
               if(!operations_stack.isEmpty())
               {
                  task.add(operations_stack.get(0));
+                 in_progress_operations_stack.add(operations_stack.get(0));
                  operations_stack.remove(operations_stack.get(0));
               }
-
             }
             thread.setTask(task);
-            in_progress_operations_stack.addAll(task);
           }
         }
       }
 
-
+    
       //Traitement de la pile d'opération : On vérifie la résolution des opérations en cours de traitement.
       Iterator it = in_progress_operations_stack.iterator();
       while (it.hasNext())
@@ -135,7 +117,7 @@ public class Client {
         {
           if(operation_inprogress.isSolved())
           {
-            this.result = (this.result + operation_inprogress.getResult())%4000;
+            this.result =(this.result+  operation_inprogress.getResult())%4000;
           }
           else
           {
@@ -143,32 +125,26 @@ public class Client {
             operations_stack.add(operation_inprogress);
 
           }
+
           it.remove();
         }
 
       }
-
+  
     }
       for (RepartiteurThread thread : threads.keySet())
       {
        thread.setInprogress(false);
-      }
+      } 
 
     System.out.println("result:"+this.result);
   }
 
-  /**
-   * Méthode permettant de récupérer le stub associé à notre serveur.
-   * @param HashMap<String, Integer>  socket Ip et port d'écoute du rmiregistry associé au serveur.
-   * @return                 Stub associé au serveur.
-   */
-	private ServerInterface loadServerStub(HashMap<String, Integer> socket)
-  {
+	private ServerInterface loadServerStub(HashMap<String, Integer> socket) {
 		ServerInterface stub = null;
 
 		try
 		{
-
 			Registry registry = LocateRegistry.getRegistry(socket.keySet().toArray()[0].toString(), socket.get(socket.keySet().toArray()[0]));
 			stub = (ServerInterface) registry.lookup("server");
 		}
@@ -191,17 +167,10 @@ public class Client {
 		return stub;
 	}
 
-  /**
-   * Méthode qui récupère toutes les opérations du fichier pour les mettre dans la pile adéquate.
-   * @param String file fichier à traiter.
-   */
-  private void FileToArray(String file)
-  {
-    try (BufferedReader lines = new BufferedReader(new FileReader("Operations/" + file)))
-    {
+  private void FileToArray(String file) {
+    try (BufferedReader lines = new BufferedReader(new FileReader("Operations/" + file))) {
       String line;
-      while ((line = lines.readLine()) != null)
-      {
+      while ((line = lines.readLine()) != null) {
         if (this._mode == 0)
           this.operations_stack.add(new Operation(line.split(" ")[0],Integer.parseInt(line.split(" ")[1]), 1));
         else if (this._mode == 1)
@@ -214,14 +183,12 @@ public class Client {
     }
   }
 
-  private void setCapacity(RepartiteurThread thread, Boolean uprate)
-  {
-    if (uprate)
-    {
-      threads.put(thread, threads.get(thread) + threads.get(thread));
-    } else
-    {
-      threads.put(thread, threads.get(thread) - threads.get(thread));
+  private void setCapacity(RepartiteurThread thread, Boolean uprate) {
+    if (uprate){
+      threads.put(thread, threads.get(thread) + 2);
+    } else {
+      threads.put(thread, threads.get(thread) - 2);
     }
+    thread.setOverload(false);
   }
 }
